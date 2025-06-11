58dc7a42e48e848d04a823ae54b79bf2e772b8ca
damanbbhardwaj1 <daman.bhardwaj@ba.com>
2025-06-11 12:45:20 +0100

    `

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-west-1a"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 }
 
